476a042e0dd9f435ef978a30937dfdea22708442
XS Development <javaist@xsd.ru>
2019-05-17 02:16:02 +0300

    Lesson 1: Answer Created Project. Added background.

---
+++ core/src/ru/xsd/lettergame/LetterGame.java
@@ -0,0 +1,33 @@
+package ru.xsd.lettergame;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class LetterGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture background;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		background = new Texture("background.jpg");
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(background, 0, 0);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		background.dispose();
+	}
+}
